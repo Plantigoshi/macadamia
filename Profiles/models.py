1e8879168e9f2ce6d97befe512118eebc1f858e3
Francisco J. Piedrahita <franciscojpiedrahita@gmail.com>
2014-09-10 08:44:24 -0500

    Added UserProfile model

--- Profiles/models.py
+++ Profiles/models.py
@@ -1,3 +1,18 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+def content_file_name(instance, filename):
+    return '/'.join(['profile', instance.user.username, 'picture'])
+
+
+class UserProfile(models.Model):
+
+    user = models.OneToOneField(User)
+
+    profile_picture = models.ImageField(null=True,
+                                        blank=True,
+                                        upload_to=content_file_name,
+                                        default='profile/default/picture.jpg')
+
+    def __unicode__(self):
+        return self.user.username
