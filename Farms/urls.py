b18548c3e0c4de191b1106383e777e88360e74b0
Francisco J. Piedrahita <franciscojpiedrahita@gmail.com>
2014-09-12 10:07:51 -0500

    Added Farm app to the macadamia project

---
+++ Farms/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import patterns, include, url
+
+from rest_framework import routers
+from rest_framework_nested import routers as nested_routers
+
+from .views import ProfileViewSet
+
+
+profile_router = routers.SimpleRouter()
+profile_router.register(r'profiles', ProfileViewSet)
+
+urlpatterns = patterns('',
+                       url(r'^', include(profile_router.urls)),)
