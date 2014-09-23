378c15a97f3a2a2a933e275ab0257ccd838b577f
Francisco J. Piedrahita <franciscojpiedrahita@gmail.com>
2014-09-23 12:18:56 -0500

    Added urls and routers

--- Messages/urls.py
+++ Messages/urls.py
@@ -2,5 +2,43 @@ from django.conf.urls import patterns, include, url
 
 from rest_framework_nested import routers
 
+from Plants.urls import user_plant_router
+from Plants.urls import plant_router
 
-urlpatterns = patterns('', )
+from .views import VariableViewSet
+
+
+user_variable_router = routers.NestedSimpleRouter(user_plant_router,
+                                                  r'user-plants',
+                                                  lookup='user-plant')
+user_variable_outer.register(r'variables',
+                             VariableViewSet,
+                             base-name='user-variables' )
+
+
+user_value_router = routers.NestedSimpleRouter(user_variable_router,
+                                               r'variables',
+                                               lookup='variable' )
+user_value_router.register(r'values',
+                           VariableViewSet,
+                           base-name='values' )
+
+variable_router = routers.NestedSimpleRouter(plant_router,
+                                             r'plants',
+                                             lookup='plant')
+variable_router.register(r'variables',
+                        VariableViewSet,
+                        base-name='variables' )
+
+value_router = routers.NestedSimpleRouter(variable_router,
+                                          r'variables',
+                                          lookup='variable' )
+value_router.register(r'values',
+                      VariableViewSet,
+                      base-name='values' )
+
+urlpatterns = patterns('',
+                       url(r'^', include(user_variable_outer.urls)),
+                       url(r'^', include(user_value_router.urls)),
+                       urls(r'^', include(variable_router.urls)),
+                       urls(r'^', include(value_router.urls)), )
